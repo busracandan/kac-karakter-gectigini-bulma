2e12f81a5e4b6545a6d71a1afb10af2c46fb4b5b
busracandan <busra58cndn@hotmail.com>
2018-02-24 12:36:02 +0200

    ikinci projem

---
+++ kaç karakter geçtığını bulan program/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace kaç_karakter_geçtığını_bulan_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    int ks;
+        //    ks = textBox1.Text.Length;
+        //    textBox2.Text = ks.ToString();
+        //}
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int ks;
+            ks = textBox1.Text.Length;
+            textBox2.Text = ks.ToString();
+
+        }
+    }
+}
